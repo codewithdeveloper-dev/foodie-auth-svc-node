00dd656678167657bbd88b0d8cd79cd2ed53dff1
PrincejebastinD <163101999+PrincejebastinD@users.noreply.github.com>
2025-05-18 15:51:36 +0530

    commit

--- Tables.sql
+++ Tables.sql
@@ -8,12 +8,14 @@ CREATE TABLE Roles (
 -- insert into roles(Roll) values('Manager');
 -- insert into roles(Roll) values('guest');
 
+
 CREATE TABLE Userlist (
   UserId SERIAL PRIMARY KEY,
   UserName VARCHAR(100),
   Email VARCHAR(100) UNIQUE NOT NULL,
   password VARCHAR(255) NOT NULL,
   RollId Numeric(10,0) DEFAULT 3,
+  AccessToken Varchar(500) ,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
--- routes/index.js
+++ routes/index.js
@@ -1,32 +1,58 @@
 var express = require('express');
 var router = express.Router();
 const connection = require('../config/connection')
-const bcrypt = require('bcrypt');
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/get', async function (req, res, next) {
-  try {
-    const responce = await connection.query('select * from Roles;');
-    res.json(responce.rows)
-  } catch (err) {
-    console.error('Connection error:', err.stack);
-    res.json(err.message)
-  }
-});
+const verifyToken = (req, res, next) => {
+  const authHeader = req.headers['authorization'];
+  if (!authHeader) return res.status(401).json({ valid: false, message: 'No token provided' });
 
+  const token = authHeader.split(' ')[1];
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    return res.status(401).json({ valid: false, message: 'Invalid token' });
+  }
+};
+
+router.post('/verifyToken', async function (req, res, next) {
+
+  const authHeader = req.headers['authorization'];
+  if (!authHeader) return res.status(401).json({ valid: false, message: 'No token provided' });
+  const token = authHeader.split(' ')[1];
+  const validtoken = await connection.query("select * from Userlist where AccessToken='" + token +"';");
+  console.log(validtoken.rows.length);
+  
+  if(validtoken.rows.length === 0) {
+    return res.status(401).json({ valid: false, message: 'Token Expired' });
+  }else{
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);  
+      req.user = decoded;
+      return res.status(200).json({valid:true,message:'Token Valid'})
+    } catch (err) {
+      return res.status(401).json({ valid: false, message: 'Invalid token' });
+    }
+
+  }
+
+});
 
 router.post('/register', async function (req, res, next) {
 
   const { UserName, Email, password, RollId } = req.body;
-  
+
   try {
     const existingUser = await connection.query("select * from Userlist where username = '" + UserName + "' and email = '" + Email + "';");
     if (existingUser.rowCount) return res.status(400).json({ message: 'Email already exists' });
-    // const hashedPassword = await bcrypt.hash(password, 10);
     const user = await connection.query("insert into Userlist(UserName,Email,password,RollId) " +
       " values('" + UserName + "','" + Email + "','" + password + "','" + RollId + "')");
 
@@ -36,5 +62,36 @@ router.post('/register', async function (req, res, next) {
   }
 });
 
+
+
+router.post('/login', async function (req, res, next) {
+  const { Email, password } = req.body;
+
+  try {
+    const sql = "select * from Userlist where password = '" + password + "' and email = '" + Email + "';"
+    const user = await connection.query(sql);
+    if (!user.rows) return res.status(404).json({ message: 'User not found' });
+    const token = jwt.sign({ Email: user.rows[0].email, password: user.rows[0].password }, process.env.JWT_SECRET);
+    const sqlquery = "Update UserList Set AccessToken = '" + token + "' where password = '" + password + "' and email = '" + Email + "';"
+    console.log(sqlquery);
+
+    const update = await connection.query(sqlquery)
+
+    res.status(200).json({ token });
+  } catch (err) {
+    res.status(500).json({ message: 'Login failed', error: err.message });
+  }
+
+});
+
+router.get('/get', verifyToken, async function (req, res, next) {
+  try {
+    const responce = await connection.query('select * from Roles;');
+    res.json(responce.rows)
+  } catch (err) {
+    console.error('Connection error:', err.stack);
+    res.json(err.message)
+  }
+});
 
 module.exports = router;
